2852a24930dd7e484c15cc338d1640ac7b1739e1
ilyas <oulkaddailyas@gmail.com>
2024-09-27 15:18:30 +0200

    fix : adapted code for on edge run

--- train.py
+++ train.py
@@ -1,19 +1,21 @@
 from concrete.compiler import parameter
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from concrete.ml import deployment
 import numpy as np
 from concrete import fhe
 from concrete.ml.deployment import FHEModelClient, FHEModelDev, FHEModelServer
 from concrete.ml.sklearn import SGDClassifier
-from concrete.ml.sklearn.base import SklearnSGDClassifierMixin
+from deepface import DeepFace
 
 
-class ModelsCluster():
-    def __init__(self) -> None:
-        self.models = {}
+class ModelTrainer():
+    def __init__(self, deploy_path: str) -> None:
+        self.model = None
+        self.facenet = DeepFace.build_model("Facenet")
+        self.deployment_path = deploy_path
 
-    def getModel(self, pubkey: str) -> SklearnSGDClassifierMixin:
-        return self.models[pubkey]
-
-    def fit(self, X: np.ndarray, y: np.ndarray, pubkey: str) -> None:
+    def fit(self, X: np.ndarray, y: np.ndarray, id: str) -> None:
         parameters_range = (-1.0, 1.0)
 
         fhe_model = SGDClassifier(
@@ -25,12 +27,21 @@ class ModelsCluster():
         )
         fhe_model.fit(X, y, fhe="execute")
         fhe_model.compile(X)
-        self.models[pubkey] = fhe_model
 
-    def predict(self, pubkey: str, emb_image: np.ndarray) -> bool:
-        model = self.getModel(pubkey)
-        res = model.predict(emb_image, fhe="execute")
-        return res[0] == 0
+        self.model = fhe_model
+
+    # def deployment(self):
+    #     DEPLOYMENT_PATH = Path(self.deployment_path)
+    #     DEPLOYMENT_PATH.mkdir(exist_ok=True)
+    #     deployment_dir = TemporaryDirectory(dir=str(DEPLOYMENT_PATH))
+    #     deployment_path = Path(deployment_dir.name)
+    #
+    #     fhe_dev = FHEModelDev(deployment_path, self.model)
+    #     fhe_dev.save(mode="training")
+    #
+    #     fhe_client = FHEModelClient(deployment_path)
+    #     fhe_client.load()
+    #     serialized_evaluation_keys = fhe_client.get_serialized_evaluation_keys()
 
 
 if __name__ == "__main__":
@@ -42,7 +53,7 @@ if __name__ == "__main__":
     """
 
     # Instantiate cluster.
-    cluster = ModelsCluster()
+    cluster = ModelTrainer("fhe_training")
 
     ################### DATASET #####################
 
@@ -59,14 +70,14 @@ if __name__ == "__main__":
 
     ################### TRAINING #####################
     # Train a model for new client.
-    pubkey = ""
-    cluster.fit(X_facenet_features, y, pubkey)
+    id = ""
+    cluster.fit(X_facenet_features, y, id)
 
     ################### TRAINING #####################
 
     # Run inference on encrypted data, Listen on server - TODO
     image = np.array([1, 2, 3])
-    response = cluster.predict(pubkey, image)
+    # response = cluster.predict(id, image)
 
     # Send response back to user - TODO
 
