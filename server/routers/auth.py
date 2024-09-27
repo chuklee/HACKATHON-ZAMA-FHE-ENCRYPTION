1dc8ce3f418b63986076cd28fa90c7550846ce75
Martin Natale <martin2.natale@epita.fr>
2024-09-27 15:41:35 +0200

    feat: Update the server return when we login

--- server/routers/auth.py
+++ server/routers/auth.py
@@ -1,11 +1,13 @@
 from typing import Literal
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from server.dependencies import auth_service
 from server.services import AuthService
 
 router = APIRouter(tags=["Auth"])
 
 
-@router.post("/login", response_model=bool, status_code=200)
+@router.post("/login", response_model=bool)
 async def login(email: str, s_service: AuthService = Depends(auth_service)) -> bool:
-    return s_service.login(email)
+    if s_service.login(email):
+        return True
+    raise HTTPException(status_code=404, detail="User not found")
