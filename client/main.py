0be122904850811905747b78521a0c41577244d6
Martin Natale <martin2.natale@epita.fr>
2024-09-27 15:10:51 +0200

    fix: Delete unused import

--- client/main.py
+++ client/main.py
@@ -2,7 +2,6 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
 from client.routers import client_router
 
 def create_app() -> FastAPI:
